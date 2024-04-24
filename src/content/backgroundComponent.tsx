43095b37512824368c8afab4fbe4409879078457
Juan Arnaboldi <nocto2014@gmail.com>
2024-04-24 13:14:33 -0300

    video assets gone

--- src/content/backgroundComponent.tsx
+++ src/content/backgroundComponent.tsx
@@ -1,7 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import React from "react"; // Importar React para acceso a React.ReactNode
 import { Button } from "@chakra-ui/react";
-// import useColorMode from "@chakra-ui/react";
 
 // Definiendo props para VideoBackground
 interface VideoBackgroundProps {
@@ -9,103 +8,85 @@ interface VideoBackgroundProps {
 }
 
 const VideoBackground: React.FC<VideoBackgroundProps> = ({ children }) => {
-    // const { colorMode } = useColorMode();
-    const videoRef = React.useRef(null);
-    const [isLoading, setIsLoading] = React.useState(true);
-
-    // Array of video sources
-    const videoSources = [
-        '/../../assets/video1.mp4',
-        '/../../assets/video2.mp4',
-        '/../../assets/video3.mp4',
+    const [currentVideoIndex, setCurrentVideoIndex] = React.useState(0);
+    const videoSources: string[] = [
+        "https://www.youtube.com/embed/NrVFAOrmSLE?autoplay=1&controls=0&loop=1&playlist=NrVFAOrmSLE&mute=1&modestbranding=1&rel=0",
+        "https://www.youtube.com/embed/tlmn8HMg2aw?autoplay=1&controls=0&loop=1&playlist=tlmn8HMg2aw&mute=1&modestbranding=1&rel=0",
+        "https://www.youtube.com/embed/avSAVXPJl0U?autoplay=1&controls=0&loop=1&playlist=avSAVXPJl0U&mute=1&modestbranding=1&rel=0",
     ];
-
-    // Random video source
-    function shuffle(array: string[]) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    const randomVideoSource = shuffle(videoSources)[0];
 
     const [isVideoForeground, setIsVideoForeground] = React.useState(false); // [1]
 
-    const handleSwitchClick = () => {
+    const handleVideoFilter = () => {
         setIsVideoForeground(!isVideoForeground);
     };
 
-    const handleMetadataLoaded = (
-        event: React.SyntheticEvent<HTMLVideoElement, Event>
+    const handleVideoSwitchClick = (
+        event: React.MouseEvent<HTMLButtonElement>
     ) => {
-        const target = event.target as HTMLVideoElement;
-        target.currentTime = Math.random() * target.duration;
-    };
-
-    const [videoSource, setVideoSource] = React.useState(randomVideoSource);
-
-    const handleVideoSwitchClick = () => {
-        const currentVideoIndex = videoSources.indexOf(videoSource);
+        event.preventDefault();
         const nextVideoIndex = (currentVideoIndex + 1) % videoSources.length;
-        setVideoSource(videoSources[nextVideoIndex]);
-        setIsLoading(true);
-        if (videoRef.current) {
-            (videoRef.current as HTMLVideoElement).load();
-        }
-        console.log("Video source changed to: ", videoSource[nextVideoIndex]);
+        setCurrentVideoIndex(nextVideoIndex);
     };
 
     return (
-        <Box height="100vh" width="100vw" overflow="hidden" position="relative">
+        <Box
+            height="100vh"
+            width="100vw"
+            overflow="hidden"
+            position="relative"
+            style={{ position: "relative", zIndex: 0 }}
+        >
             <Button
-                onClick={handleSwitchClick}
+                onClick={handleVideoSwitchClick}
                 position="absolute"
                 top="0.5rem"
                 left="4rem"
                 zIndex="2147483647"
             >
-                Switch HUD
-            </Button>
-            <Button
-                onClick={handleVideoSwitchClick}
-                position="absolute"
-                top="0.5rem"
-                left="12rem"
-                zIndex="2147483647"
-            >
                 Switch Video
             </Button>
-            <video
-                ref={videoRef}
-                autoPlay
-                loop
-                muted
-                playsInline
-                onLoadedMetadata={handleMetadataLoaded}
-                onCanPlay={() => setIsLoading(false)}
+            <Button
+                onClick={handleVideoFilter}
+                position="absolute"
+                top="0.5rem"
+                right="6rem"
+                zIndex="2147483647"
+            >
+                {isVideoForeground ? "Unblur" : "Blur"} Video
+            </Button>
+            <div
                 style={{
                     position: "absolute",
                     width: "100%",
-                    left: "50%",
-                    top: "50%",
                     height: "100%",
-                    objectFit: "cover",
-                    transform: "translate(-50%, -50%)",
-                    zIndex: isVideoForeground ? 2147483640 : -1,
-                    filter: isVideoForeground
-                        ? "contrast(1.2) saturate(1.3) brightness(1.1) sharpen(0.8)"
-                        : "contrast(1.2) saturate(1.3) brightness(1.1) blur(6px)",
-                    // "blur(6px) grayscale(0.8) brightness(0.7)
-                    transition: "opacity 1s",
-                    opacity: isLoading ? 0 : 1,
+                    top: 0,
+                    left: 0,
+                    overflow: "hidden",
+                    zIndex: -1,
+                    pointerEvents: "none",
                 }}
             >
-                <source src={randomVideoSource} type="video/mp4" />
-                Tu navegador no soporta el tag de video.
-            </video>
-            {children} {/* Renderiza los children pasados al componente */}
+                <iframe
+                    src={videoSources[currentVideoIndex]}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        width: "120vw",
+                        height: "120vh",
+                        transform: "translate(-50%, -50%) scale(1.2)",
+                        objectFit: "cover",
+                        filter: isVideoForeground
+                            ? "blur(6px)"
+                            : "contrast(120%) saturate(110%)",
+                    }}
+                ></iframe>
+            </div>
+            {children}
         </Box>
     );
 };
